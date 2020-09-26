2e1b73e9aa846cbfe205329704785deda1d8fb0c
robin <oostrobin1989@gmail.com>
2020-09-26 02:42:00 +0200

    phassener

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const app = require("./app");
 const http = require("http");
 const https = require("https");
 
-https.createServer(app).listen(process.env.PORT);
+// https.createServer(app).listen(process.env.PORT);
+
+if (typeof PhusionPassenger !== "undefined") {
+  app.listen("passenger");
+} else {
+  app.listen(3000);
+}
