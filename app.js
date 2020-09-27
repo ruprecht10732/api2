e1205947a4515cd2f140711e16cb7601d2aba8cd
robin <oostrobin1989@gmail.com>
2020-09-27 22:52:52 +0200

    wip

--- app.js
+++ app.js
@@ -21,15 +21,11 @@ app.set("view engine", "pug");
 
 app.use(cors(corsOptions));
 
-// enable files upload
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("Het werkt verdomme eindelijke");
