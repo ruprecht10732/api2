3a3008b3745969d37b1cd6fd623864cb51901895
robin <oostrobin1989@gmail.com>
2020-09-29 19:05:35 +0200

    wip

--- app.js
+++ app.js
@@ -15,13 +15,14 @@ const app = express();
 
 const corsOptions = {
   origin: "https://onboarding.thecallcompany.nl",
+  optionsSuccessStatus: 200,
 };
+
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
-app.use(cors(corsOptions));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
